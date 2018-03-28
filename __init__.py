8a183a63585ebe1609bec64692c91a631cf4423f
Ganeshbabu Thavasimuthu <gxt.gatsby@Ganeshbabus-MacBook-Pro.local>
2018-03-27 23:56:56 -0500

    new change

--- __init__.py
+++ __init__.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('hello.html', name = user)
+   
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
